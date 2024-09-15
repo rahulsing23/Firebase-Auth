a001119b2beea63843932ee3ec6b54af7791162b
rahulsing23 <rahul561singh@gmail.com>
2024-09-15 11:41:39 +0530

    firebase auth changes

--- react-firebase-auth/src/firebase.jsx
+++ react-firebase-auth/src/firebase.jsx
@@ -5,11 +5,11 @@ import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "react-firebase-auth-220a2.firebaseapp.com",
-  projectId: "react-firebase-auth-220a2",
-  storageBucket: "react-firebase-auth-220a2.appspot.com",
-  messagingSenderId: "370567851432",
-  appId: "1:370567851432:web:4b6d915b017dd7350e419d"
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
